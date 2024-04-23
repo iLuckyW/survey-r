b2b93ea56e48c5d7858ed912bd62607deec337f8
Jakob <Cramconhd@gmail.com>
2024-04-23 15:34:14 +0200

    2 neue Graficken

--- sportverhalten.R
+++ sportverhalten.R
@@ -7,26 +7,26 @@ library(dplyr)
 data <- data.frame(survey)
 
 # Dataframe erstellen, die die Häufigkeit jedes Rauchverhaltens für Männer und Frauen zählt
-smoke_counts <- as.data.frame.matrix(table(data$Sex, data$Smoke))
+exercise_counts <- as.data.frame.matrix(table(data$Sex, data$Exer))
 
 # Hinzufügen einer Spalte für das Geschlecht & Löschen der Zeilennamen
-smoke_counts$sex <- rownames(smoke_counts)
-rownames(smoke_counts) <- NULL
+exercise_counts$sex <- rownames(exercise_counts)
+rownames(exercise_counts) <- NULL
 
 # Umstrukturierung der Daten für ggplot2 in long
-smoke_counts_long <- pivot_longer(smoke_counts, cols = c("Never", "Occas", "Regul", "Heavy"), names_to = "smoke_type", values_to = "count")
+exercise_counts_long <- pivot_longer(exercise_counts, cols = c("None", "Some", "Freq",) , names_to = "exercise_type", values_to = "count")
 
 # Nach leveln sortieren
-smoke_counts_long$smoke_type <- factor(smoke_counts_long$smoke_type, levels = c("Never", "Occas", "Regul", "Heavy"))
-levels(smoke_counts_long$smoke_type) <- c("Niemals", "Gelegentlich", "Regelmäßig", "Sehr viel")
-smoke_counts_long$sex <- ifelse(smoke_counts_long$sex == "Male", "Männlich", "Weiblich")
+exercise_counts_long$exercise_type <- factor(exercise_counts_long$exercise_type, levels = c("None", "Some", "Freq"))
+levels(exercise_counts_long$exercise_type) <- c("Garnicht", "Gelegentlich", "Häufig")
+exercise_counts_long$sex <- ifelse(exercise_counts_long$sex == "Male", "Männlich", "Weiblich")
 
 # Prozente für Rauchverhalten pro Geschlecht 
-percentages <- smoke_counts_long %>% group_by(sex) %>%
+percentages <- exercise_counts_long %>% group_by(sex) %>%
   mutate(percentage = count / sum(count) * 100)
 
 # Erstellen des Balkendiagramms
-ggplot(percentages, aes(x = smoke_type, y = count, fill = sex)) +
+ggplot(percentages, aes(x = exercise_type, y = count, fill = sex)) +
   geom_bar(stat = "identity", position = "dodge") +
   geom_text(aes(label = sprintf("%.2f%%", percentage), y = count), 
             position = position_dodge(width = 0.9), vjust = -0.5, size = 3) +

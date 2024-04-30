4af043256a026231682d1a2bad66bfab5e45af96
iLuckyW <LuckyW775@gmail.com>
2024-04-30 14:17:35 +0200

    Library dplyr importiert

--- BoxplotGeschlecht&Groesse.R
+++ BoxplotGeschlecht&Groesse.R
@@ -1,17 +1,14 @@
-# Lade das Paket ggplot2
 library(ggplot2)
-library("MASS")
+library(dplyr)
+library(MASS)
 
 colnames(survey) <- c("sex", "wr_span", "nwr_span", "wr_hand", "fold_top", "pulse", "clap_top", "exercise_freq", "smoke_freq", "height", "unit", "age");
-summary(survey$age)
 
 #N.A. rausfiltern
-table<- filter(survey, survey$sex == "Male" | survey$sex == "Female")
-
+table <- filter(survey, survey$sex == "Male" | survey$sex == "Female")
 
 # Erstelle das Streudiagramm
 ggplot(table, aes(x = sex, y = height)) +
   geom_boxplot() +
   labs(x = "Geschlecht", y = "Größe") +
   theme_minimal()
-#help

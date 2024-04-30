5a0f9d1bfeb50d3fa614102a3bda6df19c376879
Jakob <Cramconhd@gmail.com>
2024-04-30 17:11:57 +0200

    Lucas ist n Lachfigut looool

--- smoke_exerc.R
+++ smoke_exerc.R
@@ -4,8 +4,12 @@ colnames(data) <- c("sex", "wr_span", "nwr_span", "wr_hand", "fold_top", "pulse"
 data$smoke_freq <- factor(data$smoke_freq, ordered = TRUE, levels = c("Never", "Occas", "Regul", "Heavy"))
 data$exercise_freq <- factor(data$exercise_freq, ordered = TRUE, levels = c("None", "Some", "Freq"))
 
+data <- data %>%
+  mutate(exercise_freq = recode(exercise_freq, "None" = "Nie", "Some" = "Gelegentlich", "Freq" = "Häufig"),
+         smoke_freq = recode(smoke_freq, "Never" = "Niemals", "Occas" = "Gelegentlich", "Regul" = "Regelmäßig", "Heavy" = "Sehr viel"))
+
 barplot(table(data$exercise_freq, data$smoke_freq), beside = TRUE, legend.text = TRUE, 
-        col = c("lightskyblue3", "cadetblue2", "steelblue2"),
-        main = "Exercise Frequency vs Smoke Frequency",
-        xlab = "Smoke Frequency", ylab = "Exercise")
+        col = c("blue3", "dodgerblue1", "cyan3"),
+        main = "Sportlichkeit vs Rauchverhalten",
+        xlab = "Rauchgewohnheit", ylab = "Sportlichkeit")
 

a40d7147971967bb098d752144248b81c3a85aec
Joel135721 <joel.daumann@hsw-stud.de>
2024-04-30 17:10:32 +0200

    WEHEMEIER

--- Streudiagramm_Puls&Alter.R
+++ Streudiagramm_Puls&Alter.R
@@ -2,12 +2,11 @@
 library(ggplot2)
 library("MASS")
 
-colnames(survey) <- c("sex", "wr_span", "nwr_span", "wr_hand", "fold_top", "pulse", "clap_top", "exercise_freq", "smoke_freq", "height", "unit", "age");
+colnames(survey) <- c("sex", "wr_span", "nwr_span", "wr_hand", "fold_top", "pulse", "clap_top", "exercise_freq", "smoke_freq", "height", "unit", "age")
 summary(survey$age)
 
-# Erstelle das Streudiagramm
-ggplot(table, aes(x = age, y = pulse)) +
-  geom_point() +
+# Erstelle das Streudiagramm und färbe alle Punkte blau ein
+ggplot(survey, aes(x = age, y = pulse)) +
+  geom_point(color = "cyan3") +
   labs(x = "Alter", y = "Puls") +
   theme_minimal()
-

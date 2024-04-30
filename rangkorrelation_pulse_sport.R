31983541915c4b970d0a588251ca79b0dd7c8d2d
iLuckyW <LuckyW775@gmail.com>
2024-04-30 17:29:24 +0200

    Danke Joel und Detlef D! Soost

--- rangkorrelation_pulse_sport.R
+++ rangkorrelation_pulse_sport.R
@@ -1,7 +1,7 @@
 library("MASS")
 library(ggplot2)
 
-colnames(survey) <- c("sex", "wr_span", "nwr_span", "wr_hand", "fold_top", "pulse", "clap_top", "exercise_freq", "smoke_freq", "height", "unit", "age");
+colnames(survey) <- c("sex", "wr_span", "nwr_span", "wr_hand", "fold_top", "pulse", "clap_top", "exercise_freq", "smoke_freq", "height", "unit", "age")
 survey$exercise_freq <- factor(survey$exercise_freq, ordered = TRUE, levels = c("None", "Some", "Freq"))
 levels(survey$exercise_freq) <- c("Niemals", "Gelegentlich", "Häufig")
 
@@ -9,5 +9,11 @@ survey$exercise_freq_rank <- rank(survey$exercise_freq)
 
 # AV -> Pulse, UV -> Sport
 lm <- lm(survey$pulse ~ survey$exercise_freq_rank)
-plot(survey$exercise_freq_rank, survey$pulse)
+
+# Punktefarben definieren
+colors <- c("Niemals" = "cyan3", "Gelegentlich" = "dodgerblue1", "Häufig" = "blue3")
+
+# Plot mit Farben für die Punkte und Legende
+plot(survey$exercise_freq_rank, survey$pulse, col = colors[survey$exercise_freq], pch = 16, xlab = "Übungshäufigkeit Rang", ylab = "Puls")
 abline(lm)
+legend("topright", legend = levels(survey$exercise_freq), col = colors, pch = 16, title = "Übungshäufigkeit")

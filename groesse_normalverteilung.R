7170c64f92c4048849a0ec65a100d95fd6548641
iLuckyW <LuckyW775@gmail.com>
2024-05-11 21:35:44 +0200

    Normalverteilung

--- groesse_normalverteilung.R
+++ groesse_normalverteilung.R
@@ -1,12 +1,17 @@
-# Laden des Datensatzes
+# Load required library
+library(ggplot2)
 library(MASS)
-library(stats)
 
-data(survey, package = "MASS")
+# Calculate mean and standard deviation of height
+clean_survey <- survey[is.finite(survey$Height), ]
 
-# Histogramm der Höhendaten
-hist(survey$Height, main = "", xlab = "Größe", ylab = "Häufigkeit", col = "dodgerblue", border = "black")
+# Calculate mean and standard deviation of height
+mean_height <- mean(clean_survey$Height)
+sd_height <- sd(clean_survey$Height)
 
-# TODO
-mean_height <- mean(survey$Height, na.rm = TRUE)
-sd_height <- sd(survey$Height, na.rm = TRUE)
+# Plot histogram of heights with Gaussian curve overlay
+ggplot(survey, aes(x = Height)) +
+  geom_histogram(aes(y=after_stat(density)), bins = 20, fill = "dodgerblue", color = "gray12") +
+  stat_function(fun = dnorm, args = list(mean = mean_height, sd = sd_height), color = "black", size = 1) +
+  theme_minimal() +
+  labs(x = "Größe", y = "Relative Häufigkeit")

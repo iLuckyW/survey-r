f6a94b9c4f063289740034db5935387150705f8a
Jakob <Cramconhd@gmail.com>
2024-05-12 12:36:29 +0200

    Lucas stinkt nach Ziegenhoden

--- kumulierteHauefigkeit_Rauchverhalten.R
+++ kumulierteHauefigkeit_Rauchverhalten.R
@@ -11,12 +11,13 @@ cumulative_sum <- cumsum(table(survey$smoke_freq))
 
 # Dataframe mit kumulativer Summe erstellen
 cumulative_df <- data.frame(smoke_freq = names(cumulative_sum), cumsum = cumulative_sum)
-
+?geom_bar
 # Plot erstellen
 ggplot(cumulative_df, aes(x = reorder(smoke_freq, cumsum), y = cumsum)) +
-  geom_bar(stat = "identity", fill = "dodgerblue") +
+  geom_bar(stat = "identity", fill = "dodgerblue", color = "dodgerblue3") +
   geom_text(aes(label = cumsum), vjust = -0.5, color = "black", size = 3) + # Add labels for absolute values
   scale_y_continuous(labels = scales::comma) + # Add commas to y-axis labels for better readability
-  labs(x = "Rauchfrequenz",
-       y = "Kumulative Anzahl") + 
+  labs(x = "Rauchverhalten",
+       y = "Kumulative Häufigkeit") + 
   theme_minimal()
+
